97f31ce5acd66d8c7285cc4fa9460a304f2cb402
Koto <koto@camra.ru>
2019-03-27 08:56:38 +0700

    Remove /js/mods/process.js in favor of .mjs version

--- js/mods/process.js
+++
@@ -1,9 +0,0 @@
-import { core } from "/js/index.js";
-console.log("in process.js core", core);
-export const process = {
-    name: "process",
-    init() {
-        core.nfo("Mod [ "+this.name+" ] init.");
-    },
-    nextTick: f => setTimeout(f, 0)
-};
